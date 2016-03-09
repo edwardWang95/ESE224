40cdc90b3eeb68dfd16cbd64535e63a9bb80de63
Edward Wang <edward.wang.95@gmail.com>
2016-03-09 18:32:43 -0500

    Setup

---
+++ ESE224/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ESE224
+//
+//  Created by Edward Wang on 3/9/16.
+//  Copyright © 2016 MidnightOwls. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
