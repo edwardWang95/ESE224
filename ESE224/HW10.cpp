4bb52985e07ed21cf3f6524d1dfca15f72be479b
Edward Wang <edwardwang@EdwardWangImac.local>
2016-04-26 02:44:27 -0400

    Setup HW10

---
+++ ESE224/HW10.cpp
@@ -0,0 +1,13 @@
+//
+//  HW10.cpp
+//  ESE224
+//
+//  Created by Edward Wang on 4/25/16.
+//  Copyright © 2016 MidnightOwls. All rights reserved.
+//
+
+#include "HW10.hpp"
+
+HW10::chapter9::chapter9(){
+    
+}
---
+++ ESE224/HW10.hpp
@@ -0,0 +1,24 @@
+//
+//  HW10.hpp
+//  ESE224
+//
+//  Created by Edward Wang on 4/25/16.
+//  Copyright © 2016 MidnightOwls. All rights reserved.
+//
+
+#ifndef HW10_hpp
+#define HW10_hpp
+
+#include <stdio.h>
+
+namespace HW10{
+    class chapter9{
+    private:
+        
+    public:
+        chapter9();
+        
+    };
+}
+
+#endif /* HW10_hpp */
